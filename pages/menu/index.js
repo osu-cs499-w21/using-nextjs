4e16c467918a23a033fabccd1610c3e3545ee060
Rob Hess <hessro@oregonstate.edu>
2021-02-24 13:59:38 -0800

    Organize code for lecture.

---
+++ pages/menu/index.js
@@ -0,0 +1,11 @@
+const menu = {
+  tacos: "https://media.giphy.com/media/KfxPgR9Xb6lRvlFa8x/giphy.gif",
+  pizza: "https://media.giphy.com/media/VCDSo9xqCJOjC/giphy.gif",
+  sushi: "https://media1.tenor.com/images/a7087e13ce68524779c9b6946818986b/tenor.gif"
+};
+
+export default function Menu() {
+  return (
+    <h1>Menu</h1>
+  );
+}
